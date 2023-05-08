4bb5f772f43c362fe50a3835447dfae3bdbff307
Vasanth1234567 <vasanthprakashvp@gmail.com>
2023-05-08 11:53:06 +0530

    Modified. Used middleware to handle request and response.

--- services.js
+++ services.js
@@ -2,27 +2,21 @@ const express=require('express')
 const router=express.Router()
 const services=[
     {
-        id:1,
         serviceName:"App Development"
     },
     {
-        id:2,
         serviceName:"Web Development"
     },
     {
-        id:3,
         serviceName:"Game Development"
     },
     {
-        id:4,
         serviceName:"UI/UX Designing"
     },
     {
-        id:5,
         serviceName:"Ethical Hacking"
     },
     {
-        id:6,
         serviceName:"Blockchain Technology"
     }
 ]
@@ -30,13 +24,10 @@ router.get("/",(request,response)=>{
     response.send("This Is Service Page")
 })
 router.get("/:id([0-9]{1,3})",(request,response)=>{
-    const id=Number(request.params.id)
-    const serviceId=services.find((service)=>service.id===id)
-    if(!serviceId){
-        response.send("Service Not Found")
-    }
-    else{
-        response.send(serviceId.serviceName)
-    }
+    response.send(request.service.serviceName)
+})
+router.param('id',(request,response,next,id)=>{
+    request.service=services[id-1]
+    next()
 })
 module.exports=router
