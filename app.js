b310a55c37fe6c51260bc02697f52afe37adada1
Vasanth1234567 <vasanthprakashvp@gmail.com>
2023-05-09 08:16:07 +0530

    Modified. Added post method.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const users=require('./users.js')
 const about=require('./aboutus.js')
 const contactus=require('./contactus.js')
 const service=require('./services.js')
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
 app.get("/",(request,response)=>{
     response.send("This is Home Page");
 })
--- services.js
+++ services.js
@@ -2,29 +2,48 @@ const express=require('express')
 const router=express.Router()
 const services=[
     {
-        serviceName:"App Development"
+        serviceName:"App Development",
+        serviceCost:"18000",
+        serviceDuration:"4 days"
     },
     {
-        serviceName:"Web Development"
+        serviceName:"Web Development",
+        serviceCost:"22000",
+        serviceDuration:"3 days"
     },
     {
-        serviceName:"Game Development"
+        serviceName:"Game Development",
+        serviceCost:"36000",
+        serviceDuration:"7 days"
     },
     {
-        serviceName:"UI/UX Designing"
+        serviceName:"UI/UX Designing",
+        serviceCost:"8000",
+        serviceDuration:"2 days"
     },
     {
-        serviceName:"Ethical Hacking"
+        serviceName:"Ethical Hacking",
+        serviceCost:"24000",
+        serviceDuration:"4 days"
     },
     {
-        serviceName:"Blockchain Technology"
+        serviceName:"Blockchain Technology",
+        serviceCost:"40000",
+        serviceDuration:"5 days"
     }
 ]
 router.get("/",(request,response)=>{
-    response.send("This Is Service Page")
+    response.send('List of Services')
 })
-router.get("/:id([0-9]{1,3})",(request,response)=>{
-    response.send(request.service.serviceName)
+router.post("/",(request,response)=>{
+    services.push({serviceName:request.body.serName,serviceCost:request.body.serCost, serviceDuration:request.body.serDuration})
+    response.redirect(`/service/${services.length}`)
+})
+router.get("/serviceform",(request,response)=>{
+    response.render("services/newservice")
+})
+router.get("/:id([0-9]{1})",(request,response)=>{
+    response.send(`Service name : ${request.service.serviceName} and  Service Cost is : ${request.service.serviceCost} and Service Duration is : ${request.service.serviceDuration}`)
 })
 router.param('id',(request,response,next,id)=>{
     request.service=services[id-1]
